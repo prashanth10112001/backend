138e38943998ff0d6cd19a9c1311c66e92bea458
prashanth10112001 <prashanthdurgam1011@gmail.com>
2025-05-09 12:18:07 +0530

    also added timestamps:true in models/Report.js

--- models/Report.js
+++ models/Report.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
-const reportSchema = new mongoose.Schema({
-  nodeValue: { type: String, default: "" },
-  startTime: { type: Date, default: null },
-  endTime: { type: Date, default: null },
-  avgAQI: { type: Number, default: 0 },
-  avgTemperature: { type: Number, default: 0 },
-  avgHumidity: { type: Number, default: 0 },
-  dominantPollutant: { type: String, default: "" },
-  powerConsumption: { type: Number, default: 0 },
-  summary: { type: String, default: "" },
-});
+const reportSchema = new mongoose.Schema(
+  {
+    nodeValue: { type: String, default: "" },
+    startTime: { type: Date, default: null },
+    endTime: { type: Date, default: null },
+    avgAQI: { type: Number, default: 0 },
+    avgTemperature: { type: Number, default: 0 },
+    avgHumidity: { type: Number, default: 0 },
+    dominantPollutant: { type: String, default: "" },
+    powerConsumption: { type: Number, default: 0 },
+    summary: { type: String, default: "" },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Report", reportSchema);
